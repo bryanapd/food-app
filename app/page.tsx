53c5cdb6ff0769ee4ea6f275d464a63ac6b18d28
Bryan <bryan.diolata@fortis.ph>
2024-08-03 02:14:50 +0800

    added slideshow

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,63 @@
-import Image from "next/image";
+import ImageSlideshow from "@/components/ImageSlideshow";
 import Link from "next/link";
 
-export default function Home() {
-  return (
+const Home = () => (
+  <>
+    <header className="flex gap-12 my-12 mx-auto w-[90%] max-w-[75rem]">
+      <div className="w-[40rem] h-[25rem]">
+        <ImageSlideshow />
+      </div>
+      <div>
+        <div className="text-[#ddd6cb] text-2xl">
+          <h1 className="text-2xl font-bold tracking-wider uppercase font-montserrat bg-gradient-text text-gradient">
+            NextLevel Food for NextLevel Foodies
+          </h1>
+          <p>Taste & share food from all over the world.</p>
+        </div>
+        <div className="flex gap-4 text-2xl [&>:first-child]:bg-transparent [&>:first-child]:font-normal [&>:first-child]:text-[#ff9b05] [&>:first-child]:active:hover:bg-transparent [&>:first-child]:pl-0 [&>:first-child]:active:hover:text-[#f9b241]">
+          <Link
+            href="/community"
+            className="inline-block mt-4 py-2 px-4 rounded-lg text-white font-bold"
+          >
+            Join the Community
+          </Link>
+          <Link
+            href="/meals"
+            className="inline-block mt-4 py-2 px-4 rounded-lg text-white font-bold bg-gradient-text hover:bg-gradient-text-hover active:bg-gradient-text-hover"
+          >
+            Explore Meals
+          </Link>
+        </div>
+      </div>
+    </header>
     <main>
-      <h1 className="text-2xl font-semibold text-white text-center">
-        Time to get started!
-      </h1>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share Meal</Link>
-      </p>
-      <p>
-        <Link href="/Community">Community</Link>
-      </p>
+      <section className="flex flex-col text-[#ddd6cb] text-2xl max-w-[50rem] w-[90%] my-8 mx-auto text-center">
+        <h2>How it works</h2>
+        <p>
+          NextLevel Food is a platform for foodies to share their favorite
+          recipes with the world. It&apos;s a place to discover new dishes, and
+          to connect with other food lovers.
+        </p>
+        <p>
+          NextLevel Food is a place to discover new dishes, and to connect with
+          other food lovers.
+        </p>
+      </section>
+
+      <section className="flex flex-col text-[#ddd6cb] text-2xl max-w-[50rem] w-[90%] my-8 mx-auto text-center">
+        <h2>Why NextLevel Food?</h2>
+        <p>
+          NextLevel Food is a platform for foodies to share their favorite
+          recipes with the world. It&apos;s a place to discover new dishes, and
+          to connect with other food lovers.
+        </p>
+        <p>
+          NextLevel Food is a place to discover new dishes, and to connect with
+          other food lovers.
+        </p>
+      </section>
     </main>
-  );
-}
+  </>
+);
+
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,18 +9,34 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
+        montserrat: ["Montserrat", "sans-serif"],
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-text": "linear-gradient(90deg, #f9572a, #ffc905)",
+        "gradient-text-hover": "linear-gradient(90deg, #fd4715, #f9b241)",
       },
-      dropShadow: {
-        custom: "0 0 0.75rem rgba(0, 0, 0, 0.5)",
+      textColor: {
+        "gradient-text": "transparent",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }: any) {
+      const newUtilities = {
+        ".text-gradient": {
+          "background-clip": "text",
+          "-webkit-background-clip": "text",
+          "text-fill-color": "transparent",
+          "-webkit-text-fill-color": "transparent",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };
+
 export default config;
